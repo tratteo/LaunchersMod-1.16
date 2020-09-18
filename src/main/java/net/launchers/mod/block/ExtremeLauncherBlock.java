e617eb97644fbec25f354f378a5e94f5221b0c5e
Matteo Beltrame <bltmtt@gmail.com>
2020-09-18 16:03:52 +0200

    init

---
+++ src/main/java/net/launchers/mod/block/ExtremeLauncherBlock.java
@@ -0,0 +1,38 @@
+package net.launchers.mod.block;
+
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.launchers.mod.block.abstraction.AbstractLauncherBlock;
+import net.launchers.mod.entity.ExtremeLauncherBlockEntity;
+import net.launchers.mod.initializer.LMSounds;
+import net.launchers.mod.loader.LMLoader;
+import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.BlockView;
+
+public class ExtremeLauncherBlock extends AbstractLauncherBlock
+{
+    public static final Identifier ID = new Identifier(LMLoader.MOD_ID, "extreme_launcher_block");
+    
+    public ExtremeLauncherBlock()
+    {
+        super(FabricBlockSettings.of(Material.METAL).breakByHand((true)).strength(2F, 1.8F).sounds(BlockSoundGroup.METAL).nonOpaque().dynamicBounds().build());
+        baseMultiplier = 2.95F;
+        stackPowerPercentage = 0.275F;
+        stackMultiplier = baseMultiplier * stackPowerPercentage;
+    }
+    
+    @Override public PlaySoundS2CPacket createLaunchSoundPacket(double x, double y, double z)
+    {
+        return new PlaySoundS2CPacket(LMSounds.LAUNCHER_BLOCK_LAUNCH_SOUNDEVENT, SoundCategory.BLOCKS, x, y, z, 1F, 0.7F);
+    }
+    
+    @Override
+    public BlockEntity createBlockEntity(BlockView view)
+    {
+        return new ExtremeLauncherBlockEntity();
+    }
+}
